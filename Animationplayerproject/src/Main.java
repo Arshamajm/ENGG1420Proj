7de6d96cc588bbbe964718994c8d597c64c19da7
Doyin <Doyin@192.168.2.37>
2022-04-02 12:56:19 -0400

    Added what main should look like

--- Animationplayerproject/src/Main.java
+++ Animationplayerproject/src/Main.java
@@ -9,6 +9,8 @@
  */
 public class Main {
     public static void main(String[] args) {
-        
+        AnimationPlayer player = new AnimationPlayer();
+        player.loadAnimationFromFile("animation.txt");
+        player.run();
     }
 }
