309a39d68f0a16e6c916841e7a1169e1930963ee
Doyin <Doyin@192.168.2.37>
2022-04-02 13:02:14 -0400

    Added skeleton of Animation player class.

---
+++ Animationplayerproject/src/AnimationPlayer.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Doyin
+ */
+public class AnimationPlayer {
+    
+    public void loadAnimationFromFile(String a){
+    
+    };
+    
+    public void run(){};
+    
+}
