eb34d17f2a2efaf673872a73f2ef4dd463f70b8d
grg-srk <grg-srk@10.0.0.107>
2022-04-02 21:36:02 -0400

    Added methods

--- Animationplayerproject/src/FXapplication.java
+++ Animationplayerproject/src/FXapplication.java
@@ -1,51 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
- */
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import java.sql.Time;
+import javax.swing.*;
 
-/**
- *
- * @author minaawad
- *  @author George
- * comments
- */
-public class FXapplication extends Application {
+
+public class FXapplication extends JPanel {
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+Time t = new Timer(5, this);
     
+    
+
+
 }
