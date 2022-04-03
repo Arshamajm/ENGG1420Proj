d1bb40fbb849dcf731700146e5b7a0347f77bc84
Mina <nimocanada@gmail.com>
2022-04-03 10:18:06 -0400

    Merge origin/class into class :)

--- Animationplayerproject/src/LLine.java
+++ Animationplayerproject/src/LLine.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
-package Draw;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
